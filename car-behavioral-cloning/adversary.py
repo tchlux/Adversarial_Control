d970f06db063df5a3653d471bd07082f3115a3a1
Thomas Lux <thomas.ch.lux@gmail.com>
2017-12-18 18:58:19 -0500

    Cleaned up adversary.py to have an easier to modify contorl flow. Generated cleaner names for adversarial images.

--- car-behavioral-cloning/adversary.py
+++ car-behavioral-cloning/adversary.py
@@ -6,15 +6,15 @@ from keras.models import load_model
 
 #      Load the images and correct steering angles     
 # =====================================================
-data_file = "images_and_steering_angles.pkl"
-with open(data_file, "rb") as f:
-    images_and_steering = pickle.load(f)
-
+DATA_FILE = "images_and_steering_angles.pkl"
+with open(DATA_FILE, "rb") as f:
+    IMAGES_AND_STEERING = pickle.load(f)
 # Load the keras model for driving     
-model = load_model("final_model.h5")
+MODEL = load_model("final_model.h5")
 
-# Global variables
-IMAGE_SHAPE = images_and_steering[0][0].shape[1:]
+#        Global variables
+# ==============================
+IMAGE_SHAPE = IMAGES_AND_STEERING[0][0].shape[1:]
 RANDOM_SEED = 0
 ADVERSARIAL_IMAGE_SIZE = tuple(np.array(IMAGE_SHAPE[:-1]) // 3)+(IMAGE_SHAPE[-1],)
 ADVERSARIAL_IMAGE_SCALE = [-1,3]
@@ -22,6 +22,18 @@ NEUTRAL_VALUE = 125
 NUM_TRANSFORMATIONS = 100
 NUM_IMAGES = 50
 MAX_COMPUTE_TIME_SEC = ((60*60)*24)
+PROGRESS_LEN = 50
+
+#      Program Control Flow     
+# ==============================
+SAVE_ALL_IMAGES = True
+NEUTRAL_IMAGE_TEST = False
+TRAIN_ON_REAL_IMAGES = False
+PLOT_DELTA_DISTRIBUTION = False
+ANALYZE_EXISTING_IMAGE = False
+GENERATE_OPTIMIZED_ADVERSARIAL_IMAGES = True
+GENERATE_CLEVERHANS_ADVERSARIAL_IMAGES = not GENERATE_OPTIMIZED_ADVERSARIAL_IMAGES
+TURN_NAME = "left"
 
 import random
 random.seed(RANDOM_SEED)
@@ -51,7 +63,7 @@ def generate_transformed_addition(addition):
 
 # A minimization objective function that evaluates the ability of an
 # adversarial image 
-def turn_left(addition):
+def turn(addition, turn_name=TURN_NAME):
     # Change the type of addition to match necessary image type
     addition = np.asarray(addition, dtype=np.uint8)
     # Reshape the addition to be the same shape as the image
@@ -63,74 +75,102 @@ def turn_left(addition):
     # addition.save("initial_addition.png")
     # Cycle N random transformations
     for transform in range(NUM_TRANSFORMATIONS):
-        print("[%s>%s]"%("="*int(round(50*transform/NUM_TRANSFORMATIONS)),
-                         " "*int(50 - round(50*transform/NUM_TRANSFORMATIONS))),end="\r")
-        transformed_addition = generate_transformed_addition(addition)
-        # Identify the change in turning angle provided by the image
-        turn = float(model.predict(np.array([transformed_addition]), batch_size=1))
-        all_delta.append(turn)
-        img = Image.fromarray(np.asarray(transformed_addition.reshape(IMAGE_SHAPE), dtype=np.uint8))
-        img_name = "left_adv_imgs/left_adversarial_%i(%.2f).png"%(transform, turn)
-        img.save(img_name)
+        print("[{:s}>{:s}]".format("-"*int(round(PROGRESS_LEN*transform/NUM_TRANSFORMATIONS)),
+                                   " "*int(PROGRESS_LEN - round(PROGRESS_LEN*transform/NUM_TRANSFORMATIONS))),end="\r")
+        if TRAIN_ON_REAL_IMAGES:
+            # Cycle all images real training images
+            random.shuffle(IMAGES_AND_STEERING)
+            for (original, sa) in IMAGES_AND_STEERING[:NUM_IMAGES]:
+                original = original.reshape(IMAGE_SHAPE)
+                # Combine the original image with the transformed addition
+                img = original + np.where(transformed_addition >= NEUTRAL_VALUE, 
+                                          transformed_addition - NEUTRAL_VALUE, 0)
+                img -= np.where(transformed_addition < NEUTRAL_VALUE,
+                                transformed_addition, 0)
+                # Identify the change in turning angle provided by the image
+                turn = float(MODEL.predict(np.array([img]), batch_size=1))
+                delta = turn - sa
+                all_delta.append(delta)
+                if SAVE_ALL_IMAGES:
+                    img = Image.fromarray(np.asarray(img.reshape(IMAGE_SHAPE), dtype=np.uint8))
+                    img_name = "{turn}_adv_imgs/({angle:+.2f})_{turn}_adversarial_{num:03d}-{orig:+.2f}_NEUTRAL.png".format(
+                        turn=turn_name, angle=turn, num=transform, orig=sa)
+                    img.save(img_name)
+        else:
+            # Train on the ability to turn
+            transformed_addition = generate_transformed_addition(addition)
+            # Identify the change in turning angle provided by the image
+            turn = float(MODEL.predict(np.array([transformed_addition]), batch_size=1))
+            all_delta.append(turn)
+            if SAVE_ALL_IMAGES:
+                img = Image.fromarray(np.asarray(transformed_addition.reshape(IMAGE_SHAPE), dtype=np.uint8))
+                img_name = "{turn}_adv_imgs/({angle:+.2f})_{turn}_adversarial_{num:03d}_NEUTRAL.png".format(
+                    turn=turn_name, angle=turn, num=transform)
+                img.save(img_name)
 
-        # # Cycle all images
-        # random.shuffle(images_and_steering)
-        # for (original, sa) in images_and_steering[:NUM_IMAGES]:
-        #     original = original.reshape(IMAGE_SHAPE)
-        #     # Combine the original image with the transformed addition
-        #     img = original + np.where(transformed_addition >= NEUTRAL_VALUE, 
-        #                               transformed_addition - NEUTRAL_VALUE, 0)
-        #     img -= np.where(transformed_addition < NEUTRAL_VALUE,
-        #                     transformed_addition, 0)
-        #     # Identify the change in turning angle provided by the image
-        #     turn = float(model.predict(np.array([img]), batch_size=1))
-        #     delta = turn - sa
-        #     all_delta.append(delta)
-    exit()
     print()
-    print(all_delta)
+
+    if SAVE_ALL_IMAGES: 
+        print("Done saving first round of images.")
+        exit()
+
+    if PLOT_DELTA_DISTRIBUTION:
+        print(all_delta)
+        from util.plotly import Plot
+        p = Plot("Randomly Transformed Adversarial {:s} Turn Image (100 bins)".format(turn_name.title()), 
+                 "Normalized Turning Angle", "Probability")
+        p.add_histogram("Turn Angles", all_delta)
+        p.plot(show=False, file_name="{}_adversarial_turn_angles.html".format(turn_name),show_legend=False)
+
     # Return the average delta achieved by all transformations on all images
     return sum(all_delta) / len(all_delta)
 
-# fake_img = np.random.randint(0,255,size=ADVERSARIAL_IMAGE_SIZE, dtype=np.uint8).flatten()
-# fake_img[:] = 125
-# print(fake_img.shape)
-# turn_left(fake_img)
+if NEUTRAL_IMAGE_TEST:
+    fake_img = np.random.randint(0,255,size=ADVERSARIAL_IMAGE_SIZE, dtype=np.uint8).flatten()
+    fake_img[:] = 125
+    print(fake_img.shape)
+    print(turn(fake_img))
 
 # ====================================
 #      Adversarial Image Analysis     
 # ====================================
+if ANALYZE_EXISTING_IMAGE:
+    with open("{}_random_solution_1.pkl".format(TURN_NAME), "rb") as f:
+        output = pickle.load(f)
 
-with open("left_random_solution_1.pkl", "rb") as f:
-    output = pickle.load(f)
+    from util.plotly import Plot
+    p = Plot()
+    p.add_histogram("Adversarial Image", output)
+    p.plot(file_name="{}_random_solution_1.html".format(TURN_NAME), show=False)
 
-from util.plotly import Plot
-p = Plot()
-p.add_histogram("Adversarial Image", output)
-p.plot(file_name="left_random_solution_1.html", show=False)
-
-print("Turn prouduced:", turn_left(output))
-img = Image.fromarray(np.asarray(output.reshape(ADVERSARIAL_IMAGE_SIZE),
-                                 dtype=np.uint8))
-# img.save("left_random_solution_1.png")
-
-exit()
+    print("Turn prouduced:", turn(output))
+    img = Image.fromarray(np.asarray(output.reshape(ADVERSARIAL_IMAGE_SIZE), dtype=np.uint8))
+    img.save("{}_random_solution_1.png".format(TURN_NAME))
+    exit()
 
 # ======================================
 #      Adversarial Image Generation     
 # ======================================
 
-from util.optimize import AMPGO, AdaptiveNormal, Random
-from util.optimize import minimize
-from best_adv import sol as initial_solution
+if GENERATE_OPTIMIZED_ADVERSARIAL_IMAGES:
+    from util.optimize import AMPGO, AdaptiveNormal, Random
+    from util.optimize import minimize
+    if TURN_NAME == "left":
+        from left_random_solution_1 import sol as initial_solution
+    else:
+        from right_random_solution_1 import sol as initial_solution
 
-sample_img = np.ones(ADVERSARIAL_IMAGE_SIZE, dtype=np.uint8).flatten()*NEUTRAL_VALUE
-print("Starting optimization.")
-bounds = [(0,255)]*np.prod(ADVERSARIAL_IMAGE_SIZE)
-output = minimize(turn_left, initial_solution, bounds=bounds,
-                  max_time=MAX_COMPUTE_TIME_SEC,
-                  method=AdaptiveNormal, display=True)
+    sample_img = np.ones(ADVERSARIAL_IMAGE_SIZE, dtype=np.uint8).flatten()*NEUTRAL_VALUE
+    print("Starting optimization.")
+    bounds = [(0,255)]*np.prod(ADVERSARIAL_IMAGE_SIZE)
+    output = minimize(turn, initial_solution, bounds=bounds,
+                      max_time=MAX_COMPUTE_TIME_SEC,
+                      method=AdaptiveNormal, display=True)
 
-print(output)
-with open("left_random_solution_1.pkl", "wb") as f:
-    pickle.dump(output, f)
+    print(output)
+    with open("{}_random_solution_1.pkl".format(TURN_NAME), "wb") as f:
+        pickle.dump(output, f)
+
+
+if GENERATE_CLEVERHANS_ADVERSARIAL_IMAGES:
+    pass
